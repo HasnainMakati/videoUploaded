4ff255c0da5bb54533aabaded8a4b5a053400d0f
Hasnain <mkthassu72@gmail.com>
2026-07-21 18:25:07 +0530

    cors issue fix

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 require("dotenv").config();
-
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const { globalErrorHandler } = require("./middleware/error.middleware.js");
+const cors = require("cors")
 const videoRoutes = require("./routes/video.routes.js");
 const adminRoutes = require("./routes/admin.routes.js");
 
 const app = express();
-
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}))
 // app.use(express.json({ limit: "16kb" }));
 // app.use(express.urlencoded({ extended: true }));
 
--- src/index.js
+++ src/index.js
@@ -1,37 +1,9 @@
 
 require("dotenv").config();
-const {app} = require("./app.js")
-const {sequelize} = require("./db/index.js")
+const { app } = require("./app.js")
+const { sequelize } = require("./db/index.js")
 
- app.listen(process.env.PORT || 4000, () => {
-            console.log(`Server running port on ${process.env.PORT}`)
-    })
+app.listen(process.env.PORT || 4000, () => {
+    console.log(`Server running port on ${process.env.PORT}`)
+})
 
-// const startServer = async () => {
-//     try {
-//         await sequelize.authenticate();
-//         console.log("Database connect successfully")
-
-//         await sequelize.sync();
-//         console.log('All models synchronized');
-
-//     } catch (error) {
-//         console.log("DB Error :", error.message)
-//         process.exit(1);
-//     }
-// }
-
-// startServer()
-
-
-// db.query("SELECT 1")
-//     .then(() => {
-//         console.log("Database connect successfully")
-
-//         app.listen(process.env.PORT || 4000, () => {
-//             console.log(`Server running port on ${process.env.PORT}`)
-//         })
-//     }).catch((err) => {
-//         console.log("Database connection failed", err);
-//         process.exit(1)
-//     });
