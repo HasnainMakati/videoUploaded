b824a7aeab882e2f3e84c0571454cb39fcd5a4d7
Hasnain <mkthassu72@gmail.com>
2026-07-27 16:03:47 +0530

    manage config-json and app.js

--- src/app.js
+++ src/app.js
@@ -8,11 +8,9 @@ const adminRoutes = require("./routes/admin.routes.js");
 
 const app = express();
 app.use(cors({
-    origin:process.env.CORS_ORIGIN,
+    origin:process.env.CORS_ORIGIN_PRO,
     credentials:true
 }))
-// app.use(express.json({ limit: "16kb" }));
-// app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ extended: true, limit: "100mb" }));
